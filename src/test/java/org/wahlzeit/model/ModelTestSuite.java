68f5c89635e33352d58c3a59b82572bdacf50e7b
12Balu34 <bastian.scherber@gmail.com>
2017-11-04 22:21:35 +0100

    - Added TestSuite classes for individual packages - Added AllSuites class that comprises all individual TestSuites

---
+++ src/test/java/org/wahlzeit/model/ModelTestSuite.java
@@ -0,0 +1,26 @@
+package org.wahlzeit.model;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.wahlzeit.model.persistence.AbstractAdapterTest;
+import org.wahlzeit.model.persistence.DatastoreAdapterTest;
+import org.wahlzeit.model.persistence.PersistenceTestSuite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AccessRightsTest.class,
+        CoordinateTest.class,
+        FlagReasonTest.class,
+        GenderTest.class,
+        GuestTest.class,
+        LocationTest.class,
+        PhotoFilterTest.class,
+        TagsTest.class,
+        UserStatusTest.class,
+        ValueTest.class,
+        PersistenceTestSuite.class
+})
+public class ModelTestSuite {
+}
